371e604262e0ff39dcc7bf556f3fbc0307b8879d
Harsh Gohil 🍃🤍🕊️ <130136657+harshintech@users.noreply.github.com>
2025-05-13 18:35:24 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./components/Header";
+import Content from "./components/content";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+    </div>
+  );
+}
